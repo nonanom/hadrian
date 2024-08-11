639bdade1a9909b01979d624ab5dab752c4b7571
Mike Vincent <nonanom@example.com>
2024-08-11 12:36:47 -0700

    fix ip

--- ec2.tf
+++ ec2.tf
@@ -22,14 +22,14 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["104.28.85.109/32"]  # mikevincent's home IP
+    cidr_blocks = ["107.197.174.39/32"]  # mikevincent's home IP
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["104.28.85.109/32"]  # mikevincent's home IP
+    cidr_blocks = ["107.197.174.39/32"]  # mikevincent's home IP
   }
 
   egress {
